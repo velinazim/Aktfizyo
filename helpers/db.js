5efd312fe7f2453936a32be2a20208a5b0561516
veli nazim <veli@digyglobal.com>
2020-12-12 21:20:21 +0300

    db connect

--- helpers/db.js
+++ helpers/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/Mydb', {
+    mongoose.connect('mongodb+srv://admin:admin@cluster0.lmfvz.mongodb.net/aktifizyo?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
